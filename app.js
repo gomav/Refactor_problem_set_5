db0a5353a679248c356a627bc1c35a315230b2cf
Manny Navarro <manny.navarro@gmail.com>
2015-05-15 11:10:57 -0600

    problem 2 solved

--- app.js
+++ app.js
@@ -8,7 +8,19 @@ var alphabetSoup = function(string){
   return alphabeticalOrder;
 };
 
-
 console.log(alphabetSoup('hello'));
 console.log(alphabetSoup('RhinoCERos'));
 console.log(alphabetSoup('TyraNNosaurus'));
+
+// Write a function vowelCount that takes a single string parameter and returns
+// the number of vowels the string contains (ie. "All cows eat grass" would
+// return 5).
+var vowelCount = function(string){
+  //using regex to isolate vowels; gi = global search "case-insensitive search" - in this case for vowels.
+  return string.match(/[aeiou]/gi).length;
+
+};
+
+console.log(vowelCount('All cows eat grass'));
+console.log(vowelCount('Tigers like to hunt'));
+console.log(vowelCount('Bears hibernate during winter, do Polar bears hibernate too?'));
